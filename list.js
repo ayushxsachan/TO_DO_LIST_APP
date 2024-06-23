2d89788fec004e88ec069e466a8a448f93c1f76c
Ayush Sachan <166961613+ayushxsachan@users.noreply.github.com>
2024-06-24 02:33:15 +0530

    Add files via upload

---
+++ list.js
@@ -0,0 +1,34 @@
+const inputbox=document.getElementById("input-box");
+const listcontainer=document.getElementById("list-container");
+function addtask(){
+    if(inputbox.value==='')
+    alert("you must write something!");
+}
+else{
+    let li=document.createElement("li");
+    li.innerHTML=inputbox.value;
+    listcontainer.appendChild(li);
+    let span=document.createElement("span");
+    span.innerHTML="\u00d7";
+    li.appendChild(span);
+}
+inputbox.value="";
+savedata();
+}
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagname==="li"){
+        e.target.classlist.toggle("checked");
+        savedata();
+    }
+    else if(e.target.tagname==="span"){
+        e.target.parentelement.remove();
+        savedata();
+    }
+},false);
+function savedata(){
+    localStorage.setItem("data",listcontainer.innerHTML);
+}
+function showtask(){
+    listcontainer.innerHTML=localStorage.getItem("data");
+}
+showtask();
